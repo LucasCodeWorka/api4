22724f7a8048853cf9bb49b530b384458ba092fe
LucasCodeWorka <154371349+LucasCodeWorka@users.noreply.github.com>
2024-05-26 13:16:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/todos', async (req, res) => {
 
 app.get("/rep", async (req, res) => {
   try {
-      const allTodos = await pool1.query('select * from public.pedidos_rep  ')
+      const allTodos = await pool1.query('select * from public.pedidos2_rep  ')
       res.json(allTodos.rows)
   } catch (err) {
       console.error(err.message)
